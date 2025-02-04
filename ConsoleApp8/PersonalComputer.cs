5dbbb1f5243cf69fb44190c3bd0d2422633f4284
zaron4k <76218026+zaron4k@users.noreply.github.com>
2025-02-04 23:07:41 +0200

    2
    2

---
+++ ConsoleApp8/PersonalComputer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class PersonalComputer
+    {
+        public string model;
+        public double cpuFrequency;
+        public int ramSize;
+        public int hddSize;
+
+        public PersonalComputer()
+        {
+            model = "Неизвестно";
+            cpuFrequency = 0.0;
+            ramSize = 0;
+            hddSize = 0;
+        }
+
+        public PersonalComputer(string model)
+        {
+            this.model = model;
+        }
+
+        public PersonalComputer(string model, double cpuFrequency) : this(model)
+        {
+            this.cpuFrequency = cpuFrequency;
+        }
+
+        public PersonalComputer(string model, double cpuFrequency, int ramSize) : this(model, cpuFrequency)
+        {
+            this.ramSize = ramSize;
+        }
+
+        public PersonalComputer(string model, double cpuFrequency, int ramSize, int hddSize) : this(model, cpuFrequency, ramSize)
+        {
+            this.hddSize = hddSize;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Модель: {model}, Тактовая частота CPU: {cpuFrequency} ГГц, RAM: {ramSize} ГБ, HDD: {hddSize} ГБ");
+        }
+
+        public override string ToString()
+        {
+            return $"PC({model}, {cpuFrequency} ГГц, {ramSize} ГБ RAM, {hddSize} ГБ HDD)";
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PersonalComputer pc1 = new PersonalComputer();
+            pc1.Info();
+            Console.WriteLine(pc1.ToString());
+
+            PersonalComputer pc2 = new PersonalComputer("Dell XPS", 3.5, 16, 512);
+            pc2.Info();
+            Console.WriteLine(pc2);
+
+            Console.Write("Введите модель ПК: ");
+            string model = Console.ReadLine();
+            Console.Write("Введите тактовую частоту процессора (ГГц): ");
+            double cpuFrequency = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите объем RAM (ГБ): ");
+            int ramSize = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите объем HDD (ГБ): ");
+            int hddSize = Convert.ToInt32(Console.ReadLine());
+
+            PersonalComputer pc3 = new PersonalComputer(model, cpuFrequency, ramSize, hddSize);
+            pc3.Info();
+            Console.WriteLine(pc3);
+        }
+    }
+}
